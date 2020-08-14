cb7e479bfe82e639aa96a865ad5371069dcb9bcd
Denis Feliciano <denisf.salles@gmail.com>
2020-08-13 23:36:06 -0300

    Adicionado estrtutura de dados de vetores, e implementado busca sequencial

---
+++ data_structures/src/com/denisfeliciano/vetor/README.md
@@ -0,0 +1,18 @@
+# Classe Vetor
+- Definição
+
+	- Vetores são estruturas de dados mais simples que existem.
+	Um vetor armazena valores, onde são todos do mesmo tipo.
+		
+- Adicionar elemento final do vetor
+- Verificar quantidade de elementos no vetor
+- Imprimir elementos do vetor
+- Obter elemento de uma posição
+- Verificar se elemento existe no vetor
+- Adicionar elemento em qualquer posição
+- Adicionar mais capacidade ao vetor
+- Remover elemento do vetor
+- Generalizar o tipo dos elemento ( string, double, int, bool, object, etc)
+- Definindo  tipo do vetor dinamicamente
+- API Java ArrayList
+- ExercÃ­cios
---
+++ data_structures/src/com/denisfeliciano/vetor/modelo/Vetor.java
@@ -0,0 +1,68 @@
+package com.denisfeliciano.vetor.modelo;
+
+import java.util.Arrays;
+
+public class Vetor {
+
+	private String[] elementos;
+	private int quantidadeItemsNoVetor = 0;
+	private int CAPACIDADE_MAX_ITEMS_VETOR;
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+		this.CAPACIDADE_MAX_ITEMS_VETOR = capacidade;
+	}
+
+	public int getCapacidadeMaximaElementosAtual() {
+		return this.CAPACIDADE_MAX_ITEMS_VETOR;
+	}
+
+	public void adiciona(String elemento) {
+		verificaSePodeAdicionarElementoNoVetor();
+
+		this.elementos[quantidadeItemsNoVetor] = elemento;
+		quantidadeItemsNoVetor++;
+
+	}
+
+	/**
+	 * 
+	 */
+	private void verificaSePodeAdicionarElementoNoVetor() {
+		if (quantidadeItemsNoVetor >= CAPACIDADE_MAX_ITEMS_VETOR)
+			throw new ArrayIndexOutOfBoundsException("Capacidade máxima do Vetor atingida!");
+	}
+
+	public Integer getQuantidadeDeItems() {
+		return this.quantidadeItemsNoVetor;
+	}
+
+	public Object getElemento(int indiceElemento) {
+		verificaSeIndiceDoVetorEvalido(indiceElemento);
+		return this.elementos[indiceElemento];
+	}
+
+	/**
+	 * @param indiceElemento
+	 */
+	private void verificaSeIndiceDoVetorEvalido(int indiceElemento) {
+		if (indiceElemento < 0 || indiceElemento >= quantidadeItemsNoVetor)
+			throw new IllegalArgumentException(
+					"Argumento invalido, o indice " + indiceElemento + " não existe no Vetor ou " + "está nulo.");
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(elementos);
+	}
+
+	public int elementExiste(String elementoProcurado) {
+		
+		for (int i=0; i< this.quantidadeItemsNoVetor; i++)
+			if (this.elementos[i].equals(elementoProcurado))
+				return i;
+		
+		return -1;
+	}
+
+}
